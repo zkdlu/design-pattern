1b20485a97e2c32bf1897bfe7d7c79afa0699dff
zkdlu <zkdlu159951@gmail.com>
2021-02-24 14:57:14 +0900

    update

--- 생성패턴/팩토리메서드.java
+++ 생성패턴/팩토리메서드.java
@@ -1,5 +1,32 @@
-package 생성패턴;
-
 public class 팩토리메서드 {
-    
+    public interface Keyboard {
+
+    }
+
+    public class LGKeyboard implements Keyboard {
+        public LGKeyboard() {
+            System.out.println("LG 키보드");
+        }
+    }
+
+    public class KeyboardFactory {
+        public Keyboard createKeyboard(String type) {
+            switch (type) {
+                case "LG":
+                    return new LGKeyboard();
+                default:
+                    return null;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        팩토리메서드 p = new 팩토리메서드();
+        p.run();
+    }
+
+    public void run() {
+        KeyboardFactory factory = new KeyboardFactory();
+        factory.createKeyboard("LG");
+    }
 }
