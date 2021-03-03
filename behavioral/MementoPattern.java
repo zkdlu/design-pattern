7b1b826810321b9bdb2fcd5d507261fdcc5baf30
zkdlu <zkdlu159951@gmail.com>
2021-03-03 17:24:49 +0900

    pre memento

---
+++ behavioral/MementoPattern.java
@@ -0,0 +1,11 @@
+package behavioral;
+
+public class MementoPattern {
+    public static void main(String[] args) {
+        new MementoPattern().run();
+    }
+
+    void run() {
+        
+    }
+}
