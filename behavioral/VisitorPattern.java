3873c3b6b3789f7d9307034d57b7fe4e67160bb7
zkdlu <zkdlu159951@gmail.com>
2021-03-08 16:53:29 +0900

    visitor pre

--- behavioral/VisitorPattern.java
+++ behavioral/VisitorPattern.java
@@ -1,5 +1,11 @@
 package behavioral;
 
 public class VisitorPattern {
-    
+    public static void main(String[] args) {
+        new VisitorPattern().run();
+    }
+
+    void run() {
+        
+    }
 }
