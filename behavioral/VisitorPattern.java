c8f5bc5169cac2a852ee202c8d566eff29739189
zkdlu <zkdlu159951@gmail.com>
2021-03-08 20:36:26 +0900

    update

--- behavioral/VisitorPattern.java
+++ behavioral/VisitorPattern.java
@@ -1,11 +1,52 @@
 package behavioral;
 
 public class VisitorPattern {
+    public interface Visitor {
+        void visit(SimpleElement element);
+        void visit(OtherElement element);
+    }
+
+    public interface Element {
+        void accept(Visitor v);
+    }
+
+    public class SimpleElement implements Element {
+        @Override
+        public void accept(Visitor v) {
+            v.visit(this);
+        }
+    }
+
+    public class OtherElement implements Element {
+        @Override
+        public void accept(Visitor v) {
+            v.visit(this);
+        }
+    }
+
+    public class SimpleVisitor implements Visitor {
+        @Override
+        public void visit(OtherElement element) {
+            System.out.println("other");
+        }
+
+        @Override
+        public void visit(SimpleElement element) {
+            System.out.println("simple");
+        }
+    }
+
     public static void main(String[] args) {
         new VisitorPattern().run();
     }
 
     void run() {
+        SimpleVisitor visitor = new SimpleVisitor();
+
+        Element simple = new SimpleElement();
+        Element other = new OtherElement();
         
+        simple.accept(visitor);
+        other.accept(visitor);
     }
 }
